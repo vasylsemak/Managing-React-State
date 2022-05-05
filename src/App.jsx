67e2aca3527b5697fdc1e23e17e46716877fd9f3
vasylsemak <vasylsmk1401@gmial.com>
2022-05-04 17:40:14 -0700

    create ErrorBoundary.js; handle error from API in .catch

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
-import Footer from './Footer'
 import Header from './Header'
-
-const products = [
-  {
-    id: 1,
-    category: 'shoes',
-    image: 'shoe1.jpg',
-    name: 'Hiker',
-    price: 94.95,
-    skus: [
-      { sku: '17', size: 7 },
-      { sku: '18', size: 8 },
-    ],
-    description: 'This rugged boot will get you up the mountain safely.',
-  },
-  {
-    id: 2,
-    category: 'shoes',
-    image: 'shoe2.jpg',
-    name: 'Climber',
-    price: 78.99,
-    skus: [
-      { sku: '28', size: 8 },
-      { sku: '29', size: 9 },
-    ],
-    description: 'Sure-footed traction in slippery conditions.',
-  },
-  {
-    id: 3,
-    category: 'shoes',
-    image: 'shoe3.jpg',
-    name: 'Explorer',
-    price: 145.95,
-    skus: [
-      { sku: '37', size: 7 },
-      { sku: '38', size: 8 },
-      { sku: '39', size: 9 },
-    ],
-    description: 'Look stylish while stomping in the mud.',
-  },
-]
+import Footer from './Footer'
+import { getProducts } from './services/productService'
 
 export default function App() {
   const [size, setSize] = useState('')
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    getProducts('shoes')
+      .then((products) => setProducts(products))
+      .catch((e) => setError(e))
+  }, [])
 
   function renderProduct(p) {
     return (
@@ -61,6 +30,8 @@ export default function App() {
   const filteredProducts = size
     ? products.filter((p) => p.skus.find((sku) => sku.size === parseInt(size)))
     : products
+
+  if (error) throw error
 
   return (
     <>
