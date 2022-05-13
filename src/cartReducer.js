93ff05f2c6b99ef68d1ba5fb7a85d77917bbf088
vasylsemak <vasylsmk1401@gmial.com>
2022-05-13 12:49:45 -0700

    add cartReducer.js: create reducer to manage cart v=with useReducer hook

---
+++ src/cartReducer.js
@@ -0,0 +1,25 @@
+export default function cartReducer(cart, action) {
+  switch (action.type) {
+    case 'add':
+      const { id, sku } = action
+      const isInCart = cart.find((i) => i.sku === sku)
+      return isInCart
+        ? cart.map((i) =>
+            i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
+          )
+        : [...cart, { id, sku, quantity: 1 }]
+
+    case 'update': {
+      const { sku, quantity } = action
+      return quantity === 0
+        ? cart.filter((i) => i.sku !== sku)
+        : cart.map((i) => (i.sku === sku ? { ...i, quantity } : i))
+    }
+
+    case 'empty':
+      return []
+
+    default:
+      throw new Error('Unhandled action' + action.type)
+  }
+}
