cc3c73e7f2e79927239c14a8268a46ffaa245702
vasylsemak <vasylsmk1401@gmial.com>
2022-05-13 09:44:26 -0700

    fix useFetchAll: prevent infinite fetch request in useEffect by comparing urls arrays

--- src/services/UseFetchAll.js
+++ src/services/UseFetchAll.js
@@ -1,11 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 export default function useFetchAll(urls) {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  // compare urls on inital render and rerender to prevent infinite fetch request
+  // cart is empty when it mounts first time
+  const prevUrls = useRef([])
 
   useEffect(() => {
+    // areEqual - f-n to compare urls arrays
+    if (areEqual(prevUrls.current, urls)) {
+      setLoading(false)
+      return
+    }
+    // if mot equal - cart arr has chenged -> new fetch request
+    prevUrls.current = urls
+
     const promises = urls.map((url) =>
       fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
         if (response.ok) return response.json()
@@ -20,8 +31,11 @@ export default function useFetchAll(urls) {
         setError(e)
       })
       .finally(() => setLoading(false))
-    // eslint-disable-next-line
-  }, [])
+  }, [urls])
 
   return { data, loading, error }
+}
+
+function areEqual(arr1, arr2) {
+  return arr1.length === arr2.length && arr1.every((el, i) => el === arr2[i])
 }
